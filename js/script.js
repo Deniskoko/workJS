726a39614c1fa75191e0229af4d903a9fee3020c
Denis <deniles@mail.ru>
2020-05-19 12:15:35 +0300

    project 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+"use strict";
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const reityng = prompt("На сколько оцените его?","");
+
+const personalMovieDB = {
+    count : numberOfFilms ,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних происмотренных фильмов?", ""),
+b = prompt("На сколько оцените его?",""),
+c = prompt("Один из последних происмотренных фильмов?", ""),
+d = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log (personalMovieDB);
