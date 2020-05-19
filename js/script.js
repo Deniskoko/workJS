544dbcf304edb6be8c8bf19c37bd810ccbfcbf85
Denis <deniles@mail.ru>
2020-05-19 15:07:56 +0300

    add cycle

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
 "use strict";
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    count : numberOfFilms ,
+    count : numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt("Один из последних происмотренных фильмов?", ""),
-b = prompt("На сколько оцените его?",""),
-c = prompt("Один из последних происмотренных фильмов?", ""),
-d = prompt("На сколько оцените его?","");
+let a,b;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i=0; i<2; i++) {
+    do {
+        a = prompt("Один из последних происмотренных фильмов?", "");
+    }
+    while (a==null||a.length==0||a.length>50);
+    b = prompt("На сколько оцените его?","");
+    // console.log (a);
+    
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count<10) {
+    alert ("Мало");
+} else if (personalMovieDB.count>=10&&personalMovieDB.count<=30) {
+    alert ("Классно");
+} else if (personalMovieDB.count>30) {
+    alert ("Киноман");
+} else {
+    alert ("Ошибка");
+}
 
 console.log (personalMovieDB);
